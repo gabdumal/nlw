c5730833776aedb696c9d9f1b972aa3e3a93c292
Gabriel Malosto <professional@gabdumal.com>
2022-10-05 01:59:30 -0300

    Corrige adição de arquivos Mobile

---
+++ mobile/App.tsx
@@ -0,0 +1,36 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Hello, world!</Text>
+      <Button title="Enviar"></Button>
+      <StatusBar style="auto" backgroundColor='purple' />
+    </View>
+  );
+}
+
+interface ButtonProps {
+  title: string;
+}
+
+function Button(props: ButtonProps) {
+  return (
+    <TouchableOpacity>
+      <Text>
+        {props.title}
+      </Text>
+    </TouchableOpacity>
+  );
+}
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
