7eb12b74375ed0fa221fde8e46fd4701fa5ee2e9
Gabriel Malosto <professional@gabdumal.com>
2022-10-05 03:46:42 -0300

    Declara rotas

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,15 +2,34 @@ import express from "express";
 
 const app = express();
 
-// localhost:3333/ads
 
-app.get("/ads", (request, response) => {
+app.get("/games", (request, response) => {
+    return response.json([
+    ]);
+});
+
+app.post("/ads", (request, response) => {
+    return response.status(201).json([
+    ]);
+});
+
+app.get("/games/:id/ads", (request, response) => {
+    const gameId = request.params.id;
+
     return response.json([
         { id: 1, name: "Anúncio 1" },
         { id: 2, name: "Anúncio 2" },
         { id: 3, name: "Anúncio 3" },
         { id: 4, name: "Anúncio 4" },
         { id: 5, name: "Anúncio 5" },
+    ]);
+});
+
+app.get("/ads/:id/discord", (request, response) => {
+    const adId = request.params.id;
+
+    return response.json([
+
     ]);
 });
 
