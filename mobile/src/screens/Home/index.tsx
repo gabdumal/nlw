0aebf7c9a711dbea31c7aecd643ac0e9a52f79bf
Gabriel Malosto <professional@gabdumal.com>
2022-10-07 01:42:44 -0300

    Cria componente GameCard //Mobile

--- mobile/src/screens/Home/index.tsx
+++ mobile/src/screens/Home/index.tsx
@@ -1,7 +1,19 @@
-import { View } from "react-native";
-
+import { View, Image } from "react-native";
 import { styles } from "./styles";
+import logoImg from "../../assets/logo-nlw-esports.png";
+import { Heading } from "../../components/Heading";
+import { GameCard } from "../../components/GameCard";
+import { GAMES } from "../../utils/games";
 
 export function Home() {
-  return <View style={styles.container}></View>;
+  return (
+    <View style={styles.container}>
+      <Image source={logoImg} style={styles.logo} />
+      <Heading
+        title="Encontre seu duo!"
+        subtitle="Selecione o game que deseja jogar..."
+      />
+      <GameCard data={GAMES[0]} />
+    </View>
+  );
 }
