631f0acf95248589b792f6d178394fd22c343fb3
Gabriel Malosto <professional@gabdumal.com>
2022-10-07 07:27:23 -0300

    Inicia implementação de notificações

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -11,12 +11,24 @@ import { Background } from "./src/components/Background";
 import { Routes } from "./src/routes";
 import { Loading } from "./src/components/Loading";
 
+import "./src/services/notificationConfigs";
+import { getPushNotificationToken } from "./src/services/getPushNotificationToken";
+import { Subscription } from "expo-modules-core";
+import { useEffect, useRef } from "react";
+
 export default function App() {
   const [fontsLoaded] = useFonts({
     Inter_400Regular,
     Inter_600SemiBold,
     Inter_700Bold,
     Inter_900Black,
+  });
+
+  const getNotificationListener = useRef<Subscription>();
+  const responseNotificationListener = useRef<Subscription>();
+
+  useEffect(() => {
+    getPushNotificationToken();
   });
 
   return (
